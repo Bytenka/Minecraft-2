b483f6da67924f72c2a79d0628a5f2d3a0dcf5f6
Bytenka <slybytenka@gmail.com>
2018-12-07 18:31:56 +0100

    Adding camera

---
+++ src/graphics/Camera.cpp
@@ -0,0 +1,48 @@
+#include "Camera.h"
+
+#include <math.h>
+#include <iostream>
+
+namespace tk
+{
+Camera::Camera(glm::dvec3 position, glm::dvec3 target)
+	: m_position(position), m_direction(glm::normalize(position - target))
+{
+	// Gets the local right vector by using the direction vector and up vector,
+	// and getting the "normal" vector of the plane created by these vectors (done with cross product)
+	//m_yaw = glm::asin(initialDir.y);
+	//m_pitch = glm::pow(glm::atan(initialDir.x, initialDir.z), 2);
+
+	m_right = glm::normalize(glm::cross(glm::dvec3(0.0, 1.0, 0.0), m_direction));
+	m_up = glm::normalize(glm::cross(m_right, m_direction));
+	m_forward = glm::normalize(glm::cross(m_right, m_up));
+}
+
+void Camera::rotate(double pitch, double yaw)
+{
+	m_yaw += yaw;
+	m_pitch -= pitch;
+
+	if (m_pitch < -89.9999)
+		m_pitch = -89.9999;
+
+	if (m_pitch > 89.9999)
+		m_pitch = 89.9999;
+
+	if (m_yaw > 180.0)
+		m_yaw -= 360.0;
+
+	if (m_yaw < -180.0)
+		m_yaw += 360.0;
+
+	glm::dvec3 newRot;
+	newRot.x = cos(glm::radians(m_pitch)) * cos(glm::radians(m_yaw));
+	newRot.y = sin(glm::radians(m_pitch));
+	newRot.z = cos(glm::radians(m_pitch)) * sin(glm::radians(m_yaw));
+
+	m_direction = glm::normalize(newRot);
+	m_right = glm::normalize(glm::cross(m_direction, glm::dvec3(0.0, 1.0, 0.0)));
+	m_up = glm::normalize(glm::cross(m_direction, m_right));
+	m_forward = glm::normalize(glm::cross(m_right, glm::dvec3(0.0, 1.0, 0.0)));
+}
+} // namespace tk
---
+++ src/graphics/Camera.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "pch.h"
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+namespace tk
+{
+class Camera
+{
+  public:
+	Camera(
+		glm::dvec3 position = glm::dvec3(0.0, 0.0, 0.0),
+		glm::dvec3 target = glm::dvec3(0.0, 0.0, -1.0));
+	~Camera(){};
+
+	void rotate(double pitch, double yaw);
+	inline void move(glm::dvec3 direction) { m_position += direction; };
+	inline void teleport(const glm::dvec3 &toLocation) { m_position = toLocation; };
+
+	inline glm::mat4 getView() const { return glm::lookAt(m_position, m_position + m_direction, glm::dvec3(0.0, 1.0, 0.0)); }
+	inline glm::dvec3 getDirection() const { return m_direction; }
+	inline glm::dvec3 getForward() const { return m_forward; }
+	inline glm::dvec3 getPosition() const { return m_position; }
+	inline glm::dvec3 getRight() const { return m_right; }
+	inline glm::dvec3 getUp() const { return m_up; }
+
+  private:
+	glm::dvec3 m_position;
+	glm::dvec3 m_direction;
+
+	glm::dvec3 m_forward; // Like direction, but it's the way, not where the player looks
+	glm::dvec3 m_right;
+	glm::dvec3 m_up;
+
+	double m_yaw = 0, m_pitch = 0;
+};
+} // namespace tk
